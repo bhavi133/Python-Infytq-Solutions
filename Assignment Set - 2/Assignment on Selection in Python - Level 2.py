e264b4b421ee51145a83c2f661ab42bfa8bad778
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-01-28 19:17:31 +0530

    Create Assignment on Selection in Python - Level 2.py

---
+++ Assignment Set - 2/Assignment on Selection in Python - Level 2.py
@@ -0,0 +1,38 @@
+Link : https://infytq.onwingspan.com/en/viewer/hands-on/lex_auth_012693780491968512136_shared?collectionId=lex_auth_0125409616243425281061_shared&collectionType=Course
+
+Problem Statement : You have x no. of 5 rupee coins and y no. of 1 rupee coins. 
+You want to purchase an item for amount z.
+The shopkeeper wants you to provide exact change. 
+You want to pay using minimum number of coins.
+How many 5 rupee coins and 1 rupee coins will you use? 
+If exact change is not possible then display -1.
+
+Solution : 
+  
+def make_amount(rupees_to_make,no_of_five,no_of_one):
+    five_needed=0
+    one_needed=0
+
+    # Start writing your code here
+    # Populate the variables: five_needed and one_needed
+    if rupees_to_make <= ((no_of_five*5)+no_of_one):
+        five_needed = rupees_to_make//5
+        if five_needed < no_of_five:
+            one_needed = rupees_to_make % 5
+            if one_needed <= no_of_one:
+                print("No. of Five needed :", five_needed)
+                print("No. of One needed  :", one_needed)
+            else:
+                print(-1)
+        else:
+            extra_five = five_needed - no_of_five
+            five_needed = five_needed - extra_five
+            one_needed = (rupees_to_make% 5) + (extra_five*5)
+            print("No. of Five needed :", five_needed)
+            print("No. of One needed  :", one_needed)
+    else:
+        print(-1)
+
+
+# Provide different values for rupees_to_make,no_of_five,no_of_one and test your program
+make_amount(28,8,5)
